d2aa8e7ed7fad80b2a3777242ba06725454f91cb
Helton Moraes <heltonbiker@gmail.com>
2020-10-19 10:09:29 -0300

    Initial code commit

---
+++ dotDep/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotDep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var slnName = args.First();
+
+            if (!File.Exists(slnName))
+            {
+                throw new FileNotFoundException($"File {slnName} not found.");
+            }
+
+            Solution solution = MSBuildWorkspace.Create()
+                .OpenSolutionAsync(slnName)
+                .Result;
+
+            //Console.WriteLine(solution.ToString());
+        }
+    }
+}
